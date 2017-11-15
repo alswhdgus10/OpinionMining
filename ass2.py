05ce50fffe922686d36a3231e2b420bd017f99a5
alswhdgus10 <alswhdgus10@gmail.com>
2017-11-15 19:24:23 +0900

    d

--- ass2.py
+++ ass2.py
@@ -10,15 +10,21 @@ def ExtractRelFromIEER(NameOfDoc, SubjClass, ObjClass, Pattern, NameOfPattern):
 
     return result
 
-pattern_list = []
 pattern_of = re.compile(r'.*\bof\b')
-pattern_list.append(pattern_of)
 pattern_in = re.compile(r'.*\bin\b')
-pattern_list.append(pattern_in)
 
+NameOfDocList = ['APW_19980314','APW_19980424','APW_19980429','NYT_19980315','NYT_19980403','NYT_19980407']
+SubjClassList = ['LOCATION','ORGANIZATION','PERSON','DURATION','DATE','CARDINAL','PERCENT','MONEY','MEASURE']
 final_result = []
-for k in pattern_list:
-    extracted = ExtractRelFromIEER('APW_19980314', 'PERSON', 'ORGANIZATION', k, 'OF')
-    final_result.append(extracted)
+
+for name in NameOfDocList:
+    for sub in SubjClassList:
+        extracted = ExtractRelFromIEER(name, sub, 'ORGANIZATION', pattern_of, 'OF')
+        final_result.append(extracted)
+        extracted = ExtractRelFromIEER(name, sub, 'ORGANIZATION', pattern_in, 'IN')
+        final_result.append(extracted)
+
+final_result=sum(final_result, [])
 
 print(final_result)
+print(len(final_result))
